e5667a28f897a5f8109d97e5ee0040766cc686f3
MarkBroerse1 <mark.broerse@student.uva.nl>
2024-06-04 16:22:34 +0200

    warning bij import_file

--- useful_functions.py
+++ useful_functions.py
@@ -21,4 +21,14 @@ def import_file(sensor_type, sensor_number):
     For sensor_number, use the one of the file, (B.253, etc)
     """
     import pandas as pd
-    return pd.read_excel(f'Data_clean/{sensor_type}_sensors/{sensor_number}_processed.xlsx')
+    import os
+    file_path = f'Data_clean/{sensor_type}_sensors/{sensor_number}_processed.xlsx'
+    
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(
+            f"Sensor_type moet met een hoofdletter beginnen!.\n"
+            f"Voor sensor_number, kijk naar hoe de file heet en gebruik daarvan dus de eerste 4 karakters'\n"
+            f"Examples: 'Data_clean/Locus_sensors/B.253_processed.xlsx', 'Data_clean/Alta_sensors/B.254_processed.xlsx'"
+        )
+    
+    return pd.read_excel(file_path)
