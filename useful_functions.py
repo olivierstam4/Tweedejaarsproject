ed5f77a0f6fb75fce9eef52129b51b79ad81c8a6
MarkBroerse1 <mark.broerse@student.uva.nl>
2024-06-05 15:59:06 +0200

    Added functions for making bins to data

--- useful_functions.py
+++ useful_functions.py
@@ -32,3 +32,56 @@ def import_file(sensor_type, sensor_number):
         )
     
     return pd.read_excel(file_path)
+
+def make_bins(data_file, num_bins, all_bins=False):
+    """
+    This function can be used to bin the data of preprocessed files
+
+    You first need to make your data a variable by using a combination of other functions
+    
+    Set all_bins to True if you want to have a seperate bin for every int in the data
+    """
+    import matplotlib.pyplot as plt
+    # Extract the 'Count' column
+    counts = data_file['Count']
+
+    if all_bins:
+        num_bins = int(counts.max())
+    else:
+        num_bins = num_bins
+
+    # Create the histograms
+    plt.hist(counts, bins=num_bins, edgecolor='black')
+
+    # Adding labels and title
+    plt.xlabel('Count')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of Counts')
+
+    # Display the histogram
+    plt.show()
+
+def make_equal_bins(data_file, num_bins=4):
+    """
+    This function can be used to bin the data of preprocessed files
+
+    You first need to make your data a variable by using a combination of other functions
+    """
+    import matplotlib.pyplot as plt
+    import numpy as np
+    # Extract the 'Count' column
+    counts = data_file['Count']
+
+    quantiles = np.linspace(0, 1, num_bins + 1)
+    bin_edges = np.quantile(counts, quantiles)
+
+    # Create the histograms
+    plt.hist(counts, bins=bin_edges, edgecolor='black')
+
+    # Adding labels and title
+    plt.xlabel('Count')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of Counts')
+
+    # Display the histogram
+    plt.show()
